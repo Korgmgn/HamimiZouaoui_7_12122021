891142c3415abc5190a04cc5e9cb7a2373ffc039
Hamimi Zouaoui <zouaoui.hm@gmail.com>
2022-02-08 13:25:40 +0100

    small fix

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,10 +2,11 @@
 
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config.js')[env];
 const db = {};
 
 let sequelize;
